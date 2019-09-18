652a4e30066ff72a3c1b8e44fe0a78a0e22e118e
liutao <876710140@qq.com>
2019-09-18 17:29:38 +0800

    修改了Hello文件的部分内容

--- Hello.java
+++ Hello.java
@@ -1,5 +1,11 @@
 public class Hello{
 	public static void main(String[] args){
+		//输出hello Word
 		System.out.println("hello word!");
+		int sum=0;
+		for(int i=0;i<5;i++){
+			sum+=i;
+		}
+		System.out.println("sum"+sum);
 	}
 }
